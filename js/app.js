68fa5085e835839c473363d823cee36cd1248f16
Ganesh Bhosale <ganesh@dwij.co.in>
2014-09-05 19:03:30 +0530

    changes

--- js/app.js
+++ js/app.js
@@ -12,3 +12,14 @@ function convertToArray(a) {
     });
     return o;
 }
+$( document ).on( "pagecreate", function() {
+    $( document ).on( "swipeleft swiperight", function( e ) {
+        if ( $( ".ui-page-active" ).jqmData( "panel" ) !== "open" ) {
+            if ( e.type === "swipeleft" ) {
+                $( "#right-panel" ).panel( "open" );
+            } else if ( e.type === "swiperight" ) {
+                $( "#left-panel" ).panel( "open" );
+            }
+        }
+    });
+});
